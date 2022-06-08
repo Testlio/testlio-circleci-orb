4283286956619161fd2e81f412b154a078e0c116
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-06-08 14:07:49 +0300

    GREEN-619: Configure requirements

--- testlio-platform-automation-main/lib/pages/base-page.ts
+++
@@ -1,7 +0,0 @@
-export default class BasePage {
-    private readonly timeout: number = 1000;
-
-    open(path: string) {
-        browser.url(path);
-    }
-}
--- testlio-platform-automation-main/types/wdio.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace WebdriverIO {
-    interface Browser {
-        waitTillElementIsLocated: (path: string, timeout: number, timeoutMsg: string) => Promise<void>;
-        findButtonWithText: (text: string) => Promise<WebdriverIO.Element>;
-        getModuleTitle: (title: string) => Promise<WebdriverIO.Element>;
-        findByElementText: (text: string) => Promise<WebdriverIO.Element>;
-    }
-}
