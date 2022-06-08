f0d97d88a7d67f820fa4beefa0ef82ffec032da7
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-06-08 15:33:08 +0300

    GREEN-619: Project refactoring

--- src/scripts/create-run.sh
+++ src/scripts/create-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export RUN_API_TOKEN=${!RUN_API_TOKEN_ENV_NAME}
-testlio create-run
+cd ${PROJECT_DIR} || exit
+testlio create-run --projectConfig ${PROJECT_CONFIG_PATH} --testConfig ${TEST_CONFIG_PATH}
--- src/scripts/package.sh
+++ src/scripts/package.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -r ${PACKAGE_NAME} ${PROJECT_DIR} -x "node_modules/*" "lib/script/*" ".git/*" ".circleci/*"
+zip -r ${PACKAGE_PATH} ${PROJECT_DIR} -x "node_modules/*" "lib/script/*" ".git/*" ".circleci/*"
