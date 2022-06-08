682ef85269bd0fbdea5255be1ffb92594f9000ee
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-06-08 16:44:53 +0300

    GREEN-619: Updated README.md

--- src/scripts/create-run.sh
+++ src/scripts/create-run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
+set -o nounset
+set -o errexit
+set -o xtrace
+
 export RUN_API_TOKEN=${!RUN_API_TOKEN_ENV_NAME}
 cd ${PROJECT_DIR} || exit
 testlio create-run --projectConfig ${PROJECT_CONFIG_PATH} --testConfig ${TEST_CONFIG_PATH}
--- src/scripts/package.sh
+++ src/scripts/package.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
+set -o nounset
+set -o errexit
+set -o xtrace
+
 cd ${PROJECT_DIR} || exit
 zip -r ${PACKAGE_PATH} . -x "node_modules/*" "lib/script/*" ".git/*" ".circleci/*"
