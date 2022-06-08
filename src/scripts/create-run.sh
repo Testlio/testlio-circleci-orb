b186063279afa5fdc47725babdcb18b2597f74c6
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-06-08 12:30:31 +0300

    GREEN-629: Test passing run api token explisitly

--- src/scripts/create-run.sh
+++ src/scripts/create-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo ${RUN_API_TOKEN}
+export RUN_API_TOKEN=${RUN_API_TOKEN_STAGING}
 cd ${PROJECT_DIR} || exit
 zip -r test_package.zip . -x "node_modules/*" "lib/script/*" ".git/*" ".circleci/*"
 testlio create-run
