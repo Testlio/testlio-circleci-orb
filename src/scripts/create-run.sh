5e6c5d733d675ac12a0b3ac67ddbd0a92cad239a
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-06-08 16:56:39 +0300

    GREEN-619: Cleanup

--- src/scripts/create-run.sh
+++ src/scripts/create-run.sh
@@ -6,4 +6,4 @@ set -o xtrace
 
 export RUN_API_TOKEN=${!RUN_API_TOKEN_ENV_NAME}
 cd ${PROJECT_DIR} || exit
-testlio create-run --projectConfig ./test --testConfig ${TEST_CONFIG_PATH}
+testlio create-run --projectConfig ${PROJECT_CONFIG_PATH} --testConfig ${TEST_CONFIG_PATH}
