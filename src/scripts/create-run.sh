95af0774a2fabcf4ea6c4348401766a55411ac9a
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-06-09 17:21:14 +0300

    GREEN-619: Get rid of packaging step

--- src/scripts/create-run.sh
+++ src/scripts/create-run.sh
@@ -4,6 +4,8 @@ set -o nounset
 set -o errexit
 set -o xtrace
 
+[ ! -f "${PROJECT_CONFIG_PATH}" ] && echo "Cannot find project config in ${PROJECT_CONFIG_PATH}." && exit
+[ ! -f "${TEST_CONFIG_PATH}" ] && echo "Cannot find test config in ${TEST_CONFIG_PATH}." && exit
+
 export RUN_API_TOKEN=${!RUN_API_TOKEN_ENV_NAME}
-cd ${PROJECT_DIR} || exit
 testlio create-run --projectConfig ${PROJECT_CONFIG_PATH} --testConfig ${TEST_CONFIG_PATH}
