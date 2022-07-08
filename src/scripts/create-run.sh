b37e45cf9f4f5d53b7629230633e75fe219b837a
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-07-08 13:58:14 +0300

    Pass testArgs properly in the script

--- src/scripts/create-run.sh
+++ src/scripts/create-run.sh
@@ -7,10 +7,12 @@ set -o xtrace
 [ ! -f "${PROJECT_CONFIG_PATH}" ] && echo "Cannot find project config in ${PROJECT_CONFIG_PATH}." && exit
 [ ! -f "${TEST_CONFIG_PATH}" ] && echo "Cannot find test config in ${TEST_CONFIG_PATH}." && exit
 
-export RUN_API_TOKEN=${!RUN_API_TOKEN_ENV_NAME}
+# export RUN_API_TOKEN=${!RUN_API_TOKEN_ENV_NAME}
 
 OPTIONAL_ARGS=()
 [ "$DRY_RUN" -eq 1 ] && OPTIONAL_ARGS+=(--dryRun)
-[ -n "$TEST_ARGS" ] && OPTIONAL_ARGS+=(--testArgs "\"${TEST_ARGS}\"")
+[ -n "$TEST_ARGS" ] && OPTIONAL_ARGS+=(--testArgs="'${TEST_ARGS}'")
+
+echo "${OPTIONAL_ARGS[@]}"
 
 testlio create-run --projectConfig "${PROJECT_CONFIG_PATH}" --testConfig "${TEST_CONFIG_PATH}" "${OPTIONAL_ARGS[@]}"
