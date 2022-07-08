a889e6ead1ca8376efe3d929d619f111a8b4659b
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-07-08 12:34:23 +0300

    Fixed issues with OPTIONAL_ARGS

--- src/scripts/create-run.sh
+++ src/scripts/create-run.sh
@@ -10,7 +10,7 @@ set -o xtrace
 export RUN_API_TOKEN=${!RUN_API_TOKEN_ENV_NAME}
 
 OPTIONAL_ARGS=""
-[ "$DRY_RUN" -eq 1 ] && OPTIONAL_ARGS=$OPTIONAL_ARGS --dryRun
-[ -n "$TEST_ARGS" ] && OPTIONAL_ARGS=$OPTIONAL_ARGS --testArgs "${TEST_ARGS}"
+[ "$DRY_RUN" -eq 1 ] && OPTIONAL_ARGS="${OPTIONAL_ARGS} --dryRun"
+[ -n "$TEST_ARGS" ] && OPTIONAL_ARGS="${OPTIONAL_ARGS} --testArgs \"${TEST_ARGS}\""
 
 testlio create-run --projectConfig "${PROJECT_CONFIG_PATH}" --testConfig "${TEST_CONFIG_PATH}" $OPTIONAL_ARGS
